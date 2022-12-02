bd3cffeec6215ce357e123aba7c2c516c49d4f93
Alina <evsyukovaalina1992@gmail.com>
2022-12-02 14:11:52 +0200

    task9 done

--- lesson21/task9/index.js
+++ lesson21/task9/index.js
@@ -1,10 +1,6 @@
 export const finishList = () => {
   const listElem = document.querySelector('ul'); //нашла
 
-  const elemList = document.createElement('li'); //создала
-  elemList.textContent = 8; //добавила текст
-  listElem.append(elemList); //вставила елемент в конец
-
   const elemList1 = document.createElement('li'); //создала
   elemList1.textContent = 1; //добавила текст
   listElem.prepend(elemList1); //вставила елемент вначало
@@ -17,6 +13,10 @@ export const finishList = () => {
   const elemList3 = document.createElement('li'); //создала
   elemList3.textContent = 6; //добавила текст
   listElemClass.after(elemList3); //вставила елемент после
+
+  const elemList = document.createElement('li'); //создала
+  elemList.textContent = 8; //добавила текст
+  listElem.append(elemList); //вставила елемент в конец
 };
 
 finishList();
