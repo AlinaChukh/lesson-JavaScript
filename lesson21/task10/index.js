0684476d348e08a948c4402c4df3ad8eefaf0cee
Alina <evsyukovaalina1992@gmail.com>
2022-12-04 13:57:37 +0200

    task10

---
+++ lesson21/task10/index.js
@@ -0,0 +1,13 @@
+const finishForm = () => {
+  const elemFormInput = document.querySelector('form');
+
+  const elemNewType = document.querySelector('input');
+  elemNewType.setAttribute('type', 'password');
+  console.log(elemNewType);
+
+  const elemNewForm = document.createElement('input');
+  elemNewForm.setAttribute('name', 'login');
+  elemFormInput.prepend(elemNewForm);
+};
+
+// finishForm();
