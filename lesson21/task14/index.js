7df211a6387b014afc3dafc1d700dbc545a02e5f
Alina <evsyukovaalina1992@gmail.com>
2022-12-06 14:39:36 +0200

    task14 done

--- lesson21/task14/index.js
+++ lesson21/task14/index.js
@@ -1,27 +1,27 @@
 export const getTitle = () => {
   const text = document.querySelector('.title');
-  console.log(text.textContent);
+  return text.textContent;
 };
 
 //getTitle();
 
 export const getDescription = () => {
   const text = document.querySelector('.about');
-  console.log(text.innerText);
+  return text.innerText;
 };
 
 //getDescription();
 
 export const getPlans = () => {
   const text = document.querySelector('.plans');
-  console.log(text.innerHTML);
+  return text.innerHTML;
 };
 
 //getPlans();
 
 export const getGoal = () => {
   const text = document.querySelector('.goal');
-  console.log(text.outerHTML);
+  return text.outerHTML;
 };
 
 //getGoal();
