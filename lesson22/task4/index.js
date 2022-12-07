f7f6f72876da767636ccb3da3226c5f4ee05a419
Alina <evsyukovaalina1992@gmail.com>
2022-12-07 22:13:03 +0200

    task4 done

---
+++ lesson22/task4/index.js
@@ -0,0 +1,8 @@
+const elemCheck = document.querySelector('.task-status');
+elemCheck.addEventListener('change', () => {
+  console.log(elemCheck.checked);
+  //   if (elemCheck.checked == 'checked') {
+  //     console.log(true);
+  //   }
+  //   console.log(false);
+});
