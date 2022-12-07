25c3fe9d23395fa0d6a5594569cbf68d8cfead37
Alina <evsyukovaalina1992@gmail.com>
2022-12-07 22:36:29 +0200

    task5 done

---
+++ lesson22/task5/index.js
@@ -0,0 +1,5 @@
+const elemBtn = document.querySelector('.search__btn');
+elemBtn.addEventListener('click', () => {
+  const textInput = document.querySelector('.search__input');
+  console.log(textInput.innerText);
+});
